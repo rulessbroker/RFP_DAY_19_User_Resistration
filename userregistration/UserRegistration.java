5e6affcf0379cc3e4aac9e4f3db79d19887e040e
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-10-02 18:08:03 +0530

    Validate first name

---
+++ userregistration/UserRegistration.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	Scanner sc = new Scanner(System.in);
+
+	public void checkFirstName() {
+		System.out.println("Enter first name start with Capital letter :");
+		String firstName = sc.nextLine();
+		if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
+			System.out.println("first name is regestried sucessfully");
+		} else {
+			System.out.println("first name pattern is not matched");
+		}
+	}
+
+	public static void main(String[] args) {
+		UserRegistration input = new UserRegistration();
+		input.checkFirstName();
+	}
+}
